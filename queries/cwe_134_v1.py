09784da144d7b0a027ea82d01a8758b5531913a0
cetfor <cetfor@gmail.com>
2017-10-13 06:55:51 -0400

    Addressed multiple mistakes and poor assumptions, added 'isa' and 'limit' statements to increase query speed on large files.

--- queries/cwe_134_v1.py
+++ queries/cwe_134_v1.py
@@ -20,28 +20,31 @@ def main(keyspace):
     graph = Graph(uri='http://localhost:4567', keyspace=keyspace)
 
     # Get address of printf to use for next query
-    query1 ='match $func isa function, has func-name contains "printf", has asm-address $a; select $a;'
+    query1 ='match $func isa function, has func-name contains "printf", has asm-address $a; offset 0; limit 100; select $a;'
     result1 = graph.execute(query1)
+    if len(result1) > 0:
+        print("Found potential calls at the following addresses:")
+        for addr in result1:
+            print(addr['a']['value'])
 
     # If printf is found continue query
     for printf_func in result1:
-        printf_addr = printf_func['a']['value']
-
         # Pull any instructions that use printf and don't use a modifier (have var type and not data type)
-        func_addr = int(result1[0]['a']['value'], 16)
-        query2 = 'match $x has operation-type "MLIL_CALL_SSA", has asm-address $a; $y isa "MLIL_CONST_PTR"; ($x,$y); $z isa constant, has constant-value {}; ($y,$z); $l isa "list", has list-size != 2; ($x,$l); $s isa "MLIL_VAR_SSA"; ($l,$s); offset 0; limit 30;select $x, $a;'.format(func_addr)
+        func_addr = int(printf_func['a']['value'], 16)
+        print("Scanning address {}".format(hex(func_addr)))
+        query2 = 'match $x isa instruction, has operation-type "MLIL_CALL_SSA", has asm-address $a; $y isa "MLIL_CONST_PTR"; ($x,$y); $z isa constant, has constant-value {}; ($y,$z); $l isa list, has list-size 1; ($x,$l); $s isa "MLIL_VAR_SSA"; ($l,$s); offset 0; limit 100; select $x, $a;'.format(func_addr)
         result2 = graph.execute(query2)
         
         # If there is an instruction that uses printf without modifier, output instruction 
         if result2:
             for instr in result2:
                 asm_addr = instr['a']['value']
-
                 print("CWE-134: Uncontrolled Format String possible at {} ".format(asm_addr))
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         keyspace = sys.argv[1]
+        main(keyspace)
     else:
-        keyspace = "grakn"
-    main(keyspace)
+        print("Please specify a keyspace to search.\nUsage: python3.6 {} <keyspace>".format(sys.argv[0]))
+
